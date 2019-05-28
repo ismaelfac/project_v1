4435d8eeba0256b5f3a0f3733350aadd8c516d50
Ismael E. Lastre Alvarez <isamelfac@gmail.com>
2019-05-27 20:46:58 -0500

    Roles y permissions

---
+++ tests/Unit/RolesPermissionTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Caffeinated\Shinobi\Models\Role;
+use Caffeinated\Shinobi\Models\Permission;
+
+class RolesPermissionTest extends TestCase
+{
+    /**
+     * @test
+     */
+    function CreateRolesPermissionsUnit()
+    {
+        $this->markTestIncomplete(true);
+    }
+}
